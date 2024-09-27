68bfe88a001d8959f12f8084e2e7ac75e0176134
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2024-09-27 12:21:34 +0300

    Initial commit

---
+++ database/migrations/2024_09_25_143222_create_exports_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exports', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name', 512);
+            $table->string('url', 512);
+            $table->string('service', 512);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exports');
+    }
+};
